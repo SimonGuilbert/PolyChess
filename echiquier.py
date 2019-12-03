a576a7a10fcc207b08ec7b8a125e15028b846bac
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2019-12-03 11:38:34 +0100

    Update echiquier.py
    Affichage du jeux d’échec + changement de reference

--- echiquier.py
+++ echiquier.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 27 15:58:02 2019
+Created on Tue Dec  3 10:43:15 2019
 
-@author: guilbers
+@author: molierej
 """
 
+
+from piece import Piece
 class Echiquier8x8:
+    '''classe qui represente l'echiquier de norme 8 x 8 '''
     # Constructor
     def __init__(self):
-        pass
-
-    # Coordonnees de chaque case de l'echiquier
-    coordonnees = [
+        
+        
+        # Coordonnees de chaque case de l'echiquier
+        self.coordonnees=[
         'a8','b8','c8','d8','e8','f8','g8','h8',
         'a7','b7','c7','d7','e7','f7','g7','h7',
         'a6','b6','c6','d6','e6','f6','g6','h6',
@@ -20,18 +23,76 @@ class Echiquier8x8:
         'a3','b3','c3','d3','e3','f3','g3','h3',
         'a2','b2','c2','d2','e2','f2','g2','h2',
         'a1','b1','c1','d1','e1','f1','g1','h1',
-        ]    
-    
-    def debutPartie(self):
-    # Position initiale des pièces sur l'echiquier
+        ]
+        
+        # Position initiale des pièces sur l'echiquier
         self.echiquier = [
-            Piece('TOUR','noir'),Piece('CAVALIER','noir'),Piece('FOU','noir'),Piece('DAME','noir'),Piece('ROI','noir'),Piece('FOU','noir'),Piece('CAVALIER','noir'),Piece('TOUR','noir'),
-            Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),
-            Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
-            Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
-            Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
-            Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
-            Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),
-            Piece('TOUR','blanc'),Piece('CAVALIER','blanc'),Piece('FOU','blanc'),Piece('DAME','blanc'),Piece('ROI','blanc'),Piece('FOU','blanc'),Piece('CAVALIER','blanc'),Piece('TOUR','blanc')
-            ]
+        Piece('TOUR','noir'),Piece('CAVALIER','noir'),Piece('FOU','noir'),Piece('DAME','noir'),Piece('ROI','noir'),Piece('FOU','noir'),Piece('CAVALIER','noir'),Piece('TOUR','noir'),
+        Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),Piece('PION','noir'),
+        Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
+        Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
+        Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
+        Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),Piece(),
+        Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),
+        Piece('TOUR','blanc'),Piece('CAVALIER','blanc'),Piece('FOU','blanc'),Piece('DAME','blanc'),Piece('ROI','blanc'),Piece('FOU','blanc'),Piece('CAVALIER','blanc'),Piece('TOUR','blanc')
+        ]
+        # matrice d'affichage
+        self.jeux=['--' for i in range(8*8) ] 
+        
+        self.tourJoueur='blanc'
+        self.historique=[]
+        
+        
+        
+        
+    def conversionEnIndex(self,position): 
+        '''passer de coordonnees en index'''
+        return self.coordonnees.index(position)
+    def pieceDeIndex(self,piece):
+        '''passer de la piece  a l'index'''
+        return self.echiquier.index(piece)
     
+    
+    def matriceJeux(self):
+        '''remplissage de la matrice d 'affichage'''
+        for index in range(len(self.echiquier)):
+            
+            if self.echiquier[index].getNom()=='TOUR':
+                self.jeux[index]='T'+ self.echiquier[index].getColour()[0]
+            elif self.echiquier[index].getNom()=='PION':
+                self.jeux[index]='P'+ self.echiquier[index].getColour()[0]
+            elif self.echiquier[index].getNom()=='FOU':
+                self.jeux[index]='F'+ self.echiquier[index].getColour()[0]
+            elif self.echiquier[index].getNom()=='CAVALIER':
+                self.jeux[index]='C'+ self.echiquier[index].getColour()[0]
+            elif self.echiquier[index].getNom()=='DAME':
+                self.jeux[index]='D'+ self.echiquier[index].getColour()[0]
+            elif self.echiquier[index].getNom()=='ROI':
+                self.jeux[index]='R'+ self.echiquier[index].getColour()[0]
+        
+    def __str__(self):
+        '''fonction d'affichage de l'objet'''
+        
+        self.matriceJeux()
+        affichage='   a  b  c  d  e  f  g  h'+'\n'
+        affichage+='  -----------------------'+'\n'
+        compteur=1
+        for index in range(len(self.jeux)):
+            if affichage[-1]=='\n':
+                affichage+=str(compteur)+'|'
+        
+            affichage+=str(self.jeux[index]) +' '
+            if self.coordonnees[index][0]=='h':
+                affichage+='|'+str(compteur)+'\n'
+                compteur+=1
+        affichage+='  -----------------------'+'\n'
+        affichage+='   a  b  c  d  e  f  g  h'
+        print(affichage)
+        
+            
+if __name__ == "__main__":  
+         
+    echiquier = Echiquier8x8()
+    
+    echiquier.__str__()
+   
