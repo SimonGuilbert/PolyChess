f8ae57573064b9027693112e201c5245f2da1cbb
Guilly-Deborah <56953554+Guilly-Deborah@users.noreply.github.com>
2019-12-03 10:53:13 +0100

    Ajout du mouvement des pions

--- piece.py
+++ piece.py
@@ -15,3 +15,33 @@ class Piece:
         self.cimetiere = []
         self.nom = nom
         self.couleur = couleur
+        
+       
+    
+    def mvmt_pion(self):
+        '''
+        Le pion peut potentiellemnt avancer de une ou deux cases devant lui au debout puis seulement une
+        ou manger une piece ennemie à gauche ou à droite en diagonale
+        Retourne la liste des positions possibles
+        '''
+        #Recuperation de la longeur de la ligne
+        ligne = echequier.get_ligne()
+        #Recuperation de la position de pion
+        position = self.echequier.get_position()
+        #Numero du tour (initial=1) DOit etre recuperer
+        nb_tours= 1
+        #Initialisation de la liste des positions
+        position_possible=[]
+        # Pion Blanc
+        if self.couleur == 'blanc':
+            if nb_tours == 1:
+                position_possible += [position-2*ligne,position-ligne,position-ligne+1,position-ligne-1]
+            else:
+                position_possible += [position-ligne,position-ligne+1,position-ligne-1]
+        #Pion noir
+        if self.couleur == 'noir':
+            if nb_tours == 1:
+                position_possible += [position+2*ligne,position+ligne,position+ligne+1,position+ligne-1]
+            else:
+                position_possible += [position+ligne,position+ligne+1,position+ligne-1]
+        return position_possible    
