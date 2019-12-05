6647c43a5e21b097d1155aab160dd4ea1de3c507
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2019-12-05 22:20:18 +0100

    Update echiquier.py
    Gestion des règles simples prise en compte

--- echiquier.py
+++ echiquier.py
@@ -2,11 +2,15 @@
 """
 Created on Tue Dec  3 10:43:15 2019
 
-@author: molierej
+@author: Jonathan Molieres
 """
 
-
+from time import sleep #permet d'attendre la lecture au joueur en cas d'une erreur de coups 
 from piece import Piece
+
+# =============================================================================
+# Classe de l'echiquier avec les regles associees 
+# =============================================================================
 class Echiquier8x8:
     '''classe qui represente l'echiquier de norme 8 x 8 '''
     # Constructor
@@ -36,21 +40,21 @@ class Echiquier8x8:
         Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),Piece('PION','blanc'),
         Piece('TOUR','blanc'),Piece('CAVALIER','blanc'),Piece('FOU','blanc'),Piece('DAME','blanc'),Piece('ROI','blanc'),Piece('FOU','blanc'),Piece('CAVALIER','blanc'),Piece('TOUR','blanc')
         ]
-        # matrice d'affichage
+        # Matrice d'affichage
         self.jeux=['--' for i in range(8*8) ] 
-        
+        # Variable qui determine les tours de jeux
         self.tourJoueur='blanc'
+        # Variable qui repertorie tout les coups joues
+        
         self.historique=[]
-        
-        
         
         
     def conversionEnIndex(self,position): 
         '''passer de coordonnees en index'''
         return self.coordonnees.index(position)
-    def pieceDeIndex(self,piece):
-        '''passer de la piece  a l'index'''
-        return self.echiquier.index(piece)
+#    def pieceDeIndex(self,piece):
+#        '''passer de la piece  a l'index'''
+#        return self.echiquier.index(piece)
     
     
     def matriceJeux(self):
@@ -58,41 +62,92 @@ class Echiquier8x8:
         for index in range(len(self.echiquier)):
             
             if self.echiquier[index].getNom()=='TOUR':
-                self.jeux[index]='T'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='T'+ self.echiquier[index].getCouleur()[0]
             elif self.echiquier[index].getNom()=='PION':
-                self.jeux[index]='P'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='P'+ self.echiquier[index].getCouleur()[0]
             elif self.echiquier[index].getNom()=='FOU':
-                self.jeux[index]='F'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='F'+ self.echiquier[index].getCouleur()[0]
             elif self.echiquier[index].getNom()=='CAVALIER':
-                self.jeux[index]='C'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='C'+ self.echiquier[index].getCouleur()[0]
             elif self.echiquier[index].getNom()=='DAME':
-                self.jeux[index]='D'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='D'+ self.echiquier[index].getCouleur()[0]
             elif self.echiquier[index].getNom()=='ROI':
-                self.jeux[index]='R'+ self.echiquier[index].getColour()[0]
+                self.jeux[index]='R'+ self.echiquier[index].getCouleur()[0]
+            elif self.echiquier[index].getNom()=='':
+                self.jeux[index]='--'
         
     def __str__(self):
-        '''fonction d'affichage de l'objet'''
+        '''fonction d'affichage du jeux d'echec  '''
         
-        self.matriceJeux()
+        self.matriceJeux()  
         affichage='   a  b  c  d  e  f  g  h'+'\n'
         affichage+='  -----------------------'+'\n'
-        compteur=1
+        compteur=8
         for index in range(len(self.jeux)):
             if affichage[-1]=='\n':
-                affichage+=str(compteur)+'|'
+                affichage+=str(compteur)
         
-            affichage+=str(self.jeux[index]) +' '
+            affichage+='|'+str(self.jeux[index])
+
             if self.coordonnees[index][0]=='h':
                 affichage+='|'+str(compteur)+'\n'
-                compteur+=1
+                compteur-=1
+        
         affichage+='  -----------------------'+'\n'
         affichage+='   a  b  c  d  e  f  g  h'
         print(affichage)
         
+        
+    def affichageHistorique(self):
+        affHist=str()
+      
+        for k in self.historique:
+            affHist+=str(k[0])+str(k[1])
+            affHist+=' | '
+        print(affHist)
+        
+#    def vide(self,index):
+#        return self.echiquier[index].getCouleur()==''
+    def deplacer(self,posInitial,posNouvelle):
+        iPosInitial=self.conversionEnIndex(posInitial)
+        iPosNouvelle=self.conversionEnIndex(posNouvelle)
+        if self.regleSimple(iPosInitial,iPosNouvelle):
             
-if __name__ == "__main__":  
+            self.echiquier[iPosNouvelle]=self.echiquier[iPosInitial]
+            self.echiquier[iPosInitial]=Piece()
+            self.historique.append([posInitial,posNouvelle])
          
-    echiquier = Echiquier8x8()
-    
-    echiquier.__str__()
+        
+        else:
+            print("Le coup n'est pas possible")
+            sleep(2)
+
+# =============================================================================
+# Mise en place des regle : 
+# =============================================================================
+    def regleSimple(self,iPosInitial,iPosNouvelle):
+        '''definie les coups non licites simple '''
+        piece=self.echiquier[iPosInitial]
+        
+        #on ne peut pas manger ses propres pieces
+        
+        if self.echiquier[iPosNouvelle].getCouleur()==self.echiquier[iPosInitial].getCouleur():
+            return False
+        
+        # verfication du coup joue par rapport a tous les coups possibles de la piece
+        if piece.getNom()=='TOUR':
+            return iPosNouvelle in piece.mvmt_tour(iPosInitial)
+        if piece.getNom()=='pion':
+            return iPosNouvelle in piece.mvmt_pion(iPosInitial)
+        if piece.getNom()=='CAVALIER':
+            return iPosNouvelle in piece.mvmt_cavalier(iPosInitial)
+        if piece.getNom()=='ROI':
+            return iPosNouvelle in piece.mvmt_roi(iPosInitial)
+        return True
+            
+#if __name__ == "__main__":  
+#         
+#    echiquier = Echiquier8x8()
+#    
+#    echiquier.__str__()
    
