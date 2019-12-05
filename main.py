5e48cb813436fcbd772f12af0b836fe01ec21769
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2019-12-05 22:22:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  2 18:32:45 2019
+
 @author: Jonathan Molieres
 """
 
@@ -20,11 +21,15 @@ if  __name__=='__main__':
         ech.__str__()
         if ech.tourJoueur=='blanc':
             print("Entrer la case du pion souhaiter  puis la case de deplacement par exemple; a8a6 permet de faire deplacer le pion en a8 en a6 ")
-            mouvement=input('entrer le mouvement:')
-            if len(mouvement)==4:
+            mouvement=input('Entrer le mouvement:')
+            if mouvement  == 'historique':
+                ech.affichageHistorique()
+                
+            elif len(mouvement)==4:
                 if mouvement=='exit': 
                     break
                 else:
                     ech.deplacer(mouvement[:2],mouvement[2:])
             
-                    
+            else:
+                print('Veuillez Recommencer ')
