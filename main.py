6d0d2f23e9fe18c1b80e3ca302350b818e678943
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2020-01-09 09:23:43 +0100

    Update main.py
    mise en place du changement joueur

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from Echiquier import Echiquier8x8
 print("\n\033[4mJeux d'echecs, exit permet d'arrêter le jeu\033[0m")
       
 if  __name__=='__main__':
+ 
     print("\nEntrer \033[31m1v1\033[0m pour jouer contre un humain et \033[31mia\033[0m pour jouer contre l'ordinateur")  
     choix = input("Choix du mode de jeu : ")
     ech=Echiquier8x8()
@@ -22,36 +23,36 @@ if  __name__=='__main__':
     if choix == "1v1" :
         while True:
             ech.__str__()
-            if ech.tourJoueur=='blanc':
-                print("\nEntrer la case de départ du pion à déplacer puis la case d'arrivée. Par exemple \033[31ma7a6\033[0m "+
-                      "permet de déplacer le pion de a7 en a6. \n\nPour roquer, il faut écrire le mot roque suivi "+
-                      "de la position de la tour que l'on souhaite 'roquer'. Par exemple \033[31mroqueh1\033[0m")
-                mouvement=input('Entrer le mouvement (exit permet de quitter) : ')
-                if mouvement  == 'historique':
-                    ech.affichageHistorique()
-                elif len(mouvement)==7 and mouvement[:5] == "roque":
-                    try: 
-                        ech.roque(mouvement[-2:])
-                    except:
-                        print("\033[31mCommande non valide. Veuillez Recommencer\033[0m")
-                        sleep(2)
-                        
-                elif len(mouvement)==4:
-                    if mouvement=='exit': 
-                        break
-                    else:
-                        try:
-                            ech.deplacer(mouvement[:2],mouvement[2:])
-                            nbPosRoi=ech.echec()
-                            if nbPosRoi!=0 and nbPosRoi!=-1:
-                                print('echec')
-                        except:
-                            print("\033[31mCommande non valide. Veuillez Recommencer\033[0m")
-                            sleep(2)
-                            
-                
-                else:
-                    print("\033[31mCommande non valide. Veuillez Recommencer\033[0m")
+           
+            print("\nEntrer la case de départ du pion à déplacer puis la case d'arrivée. Par exemple \033[31ma7a6\033[0m "+
+                  "permet de déplacer le pion de a7 en a6. \n\nPour roquer, il faut écrire le mot roque suivi "+
+                  "de la position de la tour que l'on souhaite 'roquer'. Par exemple \033[31mroqueh1\033[0m")
+            mouvement=input('Aux '+str(ech.tourJoueur)+'s de jouer (exit permet de quitter) : ')
+            if mouvement  == 'historique':
+                ech.affichageHistorique()
+            elif len(mouvement)==7:
+                try: 
+                    ech.roque(mouvement[-2:])
+                except:
+                    print("\033[31mCommande non valide. Veuillez Recommencer1\033[0m")
                     sleep(2)
+                    
+            elif len(mouvement)==4:
+                if mouvement=='exit': 
+                    break
+                else:
+                    try:
+                        ech.deplacer(mouvement[:2],mouvement[2:])
+                        
+#                        nbPosRoi=ech.echec()
+#                        if nbPosRoi!=0 and nbPosRoi!=-1:
+#                            print('echec')
+                    except:
+                        print("\033[31mCommande non valide. Veuillez Recommencer2\033[0m")              
+                        sleep(2)    
+            else:
+                print("\033[31mCommande non valide. Veuillez Recommencer3\033[0m")
+                sleep(2)
+            
     elif choix == "ia" :
         print("Le mode de jeu contre une intelligence artificielle n'est pas encore disponible")
